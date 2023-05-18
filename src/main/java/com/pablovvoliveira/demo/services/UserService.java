a2d3c7dd128fdcf9a34211d29c156ca39aa7c1e5
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-05-18 10:18:13 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/pablovvoliveira/demo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.pablovvoliveira.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pablovvoliveira.demo.domain.User;
+import com.pablovvoliveira.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
