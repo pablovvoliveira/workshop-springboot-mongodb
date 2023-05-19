59362ea466ace76f8d5cacb4b6be3d212e57619d
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-05-19 10:41:27 -0300

    Getting post by id

---
+++ src/main/java/com/pablovvoliveira/demo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.pablovvoliveira.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pablovvoliveira.demo.domain.Post;
+import com.pablovvoliveira.demo.repositories.PostRepository;
+import com.pablovvoliveira.demo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+		
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+}
